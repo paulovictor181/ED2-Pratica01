de2e1f1f5f7a66be4abf33ceb4a9bbb1f6dcfed8
paulovictor181 <paulo_victor_cc82@outlook.com>
2025-05-18 22:35:27 -0300

    Lista Ligado do projeto criada

--- enteties/RegistroClimatico.java
+++ enteties/RegistroClimatico.java
@@ -3,11 +3,99 @@ package enteties;
 import java.time.LocalDate;
 
 public class RegistroClimatico {
-    int idRegistro;
-    String idDispositivo;
-    LocalDate dataHora;
-    double temperatura;
-    double umidade;
-    double pressao;
+    private int idRegistro;
+    private String idDispositivo;
+    private LocalDate dataHora;
+    private double temperatura;
+    private double umidade;
+    private double pressao;
     RegistroClimatico proximo;
+
+    public RegistroClimatico(int idRegistro, double pressao, double umidade, double temperatura, LocalDate dataHora, String idDispositivo) {
+        this.idRegistro = idRegistro;
+        this.pressao = pressao;
+        this.umidade = umidade;
+        this.temperatura = temperatura;
+        this.dataHora = dataHora;
+        this.idDispositivo = idDispositivo;
+        this.proximo = null;
+    }
+
+    public RegistroClimatico(int idRegistro, RegistroClimatico proximo, double pressao, double umidade, double temperatura, LocalDate dataHora, String idDispositivo) {
+        this.idRegistro = idRegistro;
+        this.proximo = proximo;
+        this.pressao = pressao;
+        this.umidade = umidade;
+        this.temperatura = temperatura;
+        this.dataHora = dataHora;
+        this.idDispositivo = idDispositivo;
+    }
+
+    public int getIdRegistro() {
+        return idRegistro;
+    }
+
+    public void setIdRegistro(int idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    public String getIdDispositivo() {
+        return idDispositivo;
+    }
+
+    public void setIdDispositivo(String idDispositivo) {
+        this.idDispositivo = idDispositivo;
+    }
+
+    public LocalDate getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDate dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public double getUmidade() {
+        return umidade;
+    }
+
+    public void setUmidade(double umidade) {
+        this.umidade = umidade;
+    }
+
+    public double getPressao() {
+        return pressao;
+    }
+
+    public void setPressao(double pressao) {
+        this.pressao = pressao;
+    }
+
+    public RegistroClimatico getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(RegistroClimatico proximo) {
+        this.proximo = proximo;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistroClimatico {\n" +
+                "  idRegistro    = " + idRegistro + "\n" +
+                "  idDispositivo = '" + idDispositivo + "'\n" +
+                "  dataHora      = " + dataHora + "\n" +
+                "  temperatura   = " + temperatura + "\n" +
+                "  umidade       = " + umidade + "\n" +
+                "  pressao       = " + pressao + "\n" +
+                "}";
+    }
 }
---
+++ utilities/ListaLigadaRegistros.java
@@ -0,0 +1,152 @@
+package utilities;
+
+import enteties.RegistroClimatico;
+
+public class ListaLigadaRegistros {
+    private RegistroClimatico primeiro;
+    private RegistroClimatico ultimo;
+    private long tamanho;
+
+    public ListaLigadaRegistros() {
+        this.primeiro = null;
+        this.ultimo = null;
+        this.tamanho = 0;
+    }
+
+    public void listar(){
+        RegistroClimatico registro = primeiro;
+
+        if(registro == null){
+            System.out.println("Banco Vazio!!!");
+        } else {
+            while(registro != null){
+                System.out.println(registro);
+                registro = registro.getProximo();
+            }
+        }
+
+    }
+
+    public void inserir(RegistroClimatico registro){
+        RegistroClimatico registroInserido = registro;
+        if(primeiro == null){
+            primeiro = registroInserido;
+            ultimo = registroInserido;
+        } else {
+            ultimo.setProximo(registroInserido);
+            ultimo = registroInserido;
+        }
+        tamanho++;
+    }
+
+    public RegistroClimatico buscar(int IdRegistro){
+        RegistroClimatico registro = primeiro;
+
+        while(registro != null){
+            if (registro.getIdRegistro() == IdRegistro){
+                return registro;
+            }
+            registro = registro.getProximo();
+        }
+        return null;
+    }
+
+    public RegistroClimatico buscarAnterior(int IdRegistro){
+        RegistroClimatico registro = primeiro;
+        RegistroClimatico registroAnterior = null;
+
+        while(registro != null){
+            registroAnterior = registro;
+            registro = registro.getProximo();
+
+
+            if (registro.getIdRegistro() == IdRegistro){
+                return registroAnterior;
+            }
+        }
+
+        return null;
+    }
+
+    public RegistroClimatico removerPrimeiro(){
+        RegistroClimatico registro = primeiro;
+        RegistroClimatico registroRemovido = null;
+
+        if(primeiro == null){
+            System.out.println("Banco Vazio!!!");
+        } else {
+
+            registroRemovido = primeiro;
+
+            if(primeiro == ultimo){
+                primeiro = null;
+                ultimo = null;
+            } else {
+                primeiro = primeiro.getProximo();
+            }
+            registro.setProximo(null);
+
+            tamanho--;
+        }
+
+        return registroRemovido;
+    }
+
+    public RegistroClimatico removerUltimo(){
+        RegistroClimatico registroRemovido = null;
+
+        if(ultimo == null){
+            System.out.println("Banco Vazio!!!");
+        } else {
+
+            registroRemovido = ultimo;
+
+            if(primeiro == ultimo){
+                primeiro = null;
+                ultimo = null;
+            } else {
+                RegistroClimatico registro = primeiro;
+
+                while(registro.getProximo() != ultimo){
+                    registro = registro.getProximo();
+                }
+
+                ultimo = registro;
+                ultimo.setProximo(null);
+            }
+            tamanho--;
+        }
+
+        return registroRemovido;
+    }
+
+    public RegistroClimatico remover(int IdRegistro){
+        RegistroClimatico anterior = null;
+        RegistroClimatico removido = null;
+
+        if(primeiro == null){
+            return null;
+        }
+
+        RegistroClimatico registroAnterior = buscarAnterior(IdRegistro);
+
+        if(registroAnterior == null){
+            if(primeiro.getIdRegistro() == IdRegistro){
+                return removerPrimeiro();
+            } else {
+                return null;
+            }
+        } else {
+            removido = registroAnterior.getProximo();
+
+            if(removido == ultimo){
+                return removerUltimo();
+            } else {
+                registroAnterior.setProximo(removido.getProximo());
+                removido.setProximo(null);
+                tamanho--;
+                return removido;
+            }
+        }
+    }
+}
